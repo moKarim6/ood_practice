4073fe0c627888daf93a09c53327ce9a748f2665
moKarim6 <60603207+moKarim6@users.noreply.github.com>
2021-05-11 01:17:34 +0100

    1st commit

---
+++ WpfApp1/Phone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Phone
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OSImage { get; set; }
+        public string PhoneImage { get; set; }
+    }
+}
